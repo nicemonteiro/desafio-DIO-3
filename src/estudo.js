f41cb18d8ea48c06e8e09f395b1975475e9c39d5
nicemonteiro <149893253+nicemonteiro@users.noreply.github.com>
2026-01-16 15:45:10 -0300

    Delete src/estudo.js

--- src/estudo.js
+++
@@ -1,9 +0,0 @@
-let userName = getFirsName ("Eunice Nunes Monteiro")
-
-function getFirsName (name){
-    let firstName = name.split (" ") [0 && 2]
-    return firstName
-}
-
-
-console.log("Seja bem-vinda, " + userName)
